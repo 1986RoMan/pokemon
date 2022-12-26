a46df935b7dd385cb4e5ff12a93c71d78fa996b7
partyzan1986 <partyzan1986@gmail.com>
2022-12-26 18:46:40 +0200

    project

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -3,6 +3,7 @@ import React, {FC} from 'react';
 import {useAppDispatch, useAppSelector} from "../hooks/hook";
 
 const SearchPage:FC = () => {
+
     const dispatch = useAppDispatch();
     const {pokemon} = useAppSelector(state => state.pokemonReducer);
     console.log(pokemon)
